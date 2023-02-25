60ad62174530cad58c5b1c29440c021e509cdcfc
aayamit <aayamit@gmail.com>
2023-02-25 16:55:05 +0530

    now

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,64 +21,64 @@ const seo = {
 const greeting = {
   title: "Amit Kumar",
   logo_name: "AmitKumar",
-  nickname: "crypto_enthusiastr",
+  nickname: "crypto_enthusiast",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/1FL_wydnPF2SWCdw2goYAgZ1KKBx8TpcD/view?usp=sharing",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+    "https://drive.google.com/file/d/1064lqtkf3diCM0qm8IVSyszZ49mBZBZ9/view?usp=sharing",
+  portfolio_repository: "https://github.com/aayamit",
   githubProfile: "https://github.com/aayamit",
 };
 
 const socialMediaLinks = [
   /* Your Social Media Link */
-  // github: "https://github.com/ashutosh1919",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-  // gmail: "ashutoshhathidara98@gmail.com",
-  // gitlab: "https://gitlab.com/ashutoshhathidara98",
-  // facebook: "https://www.facebook.com/laymanbrother.19/",
-  // twitter: "https://twitter.com/ashutosh_1919",
-  // instagram: "https://www.instagram.com/layman_brother/"
+  // github: "https://github.com/aayamit",
+  // linkedin: "https://www.linkedin.com/in/aayamit/",
+  // gmail: "aayamit@gmail.com",
+  // gitlab: "https://gitlab.com/aayamit",
+  // facebook: "https://www.facebook.com/amitkshk/",
+  // twitter: "https://twitter.com/samianyr",
+  // instagram: "https://www.instagram.com/amitkrz/"
 
   {
     name: "Github",
-    link: "https://github.com/ashutosh1919",
+    link: "https://github.com/aayamit",
     fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
     backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/ashutosh1919/",
+    link: "https://www.linkedin.com/in/aayamit/",
     fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
   },
   {
     name: "YouTube",
-    link: "https://youtube.com/c/DevSense19",
+    link: "https://youtube.com/c/patnadaily",
     fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
     backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
   },
   {
     name: "Gmail",
-    link: "mailto:ashutoshhathidara98@gmail.com",
+    link: "mailto:aayamit@gmail.com",
     fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
     backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
   },
   {
     name: "Twitter",
-    link: "https://twitter.com/ashutosh_1919",
+    link: "https://twitter.com/samianyr",
     fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
     backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
   },
   {
     name: "Facebook",
-    link: "https://www.facebook.com/laymanbrother.19/",
+    link: "https://www.facebook.com/amitkshk/",
     fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
     backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
   },
   {
     name: "Instagram",
-    link: "https://www.instagram.com/layman_brother/",
+    link: "https://www.instagram.com/amitkrz/",
     fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
     backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
   },
@@ -328,7 +328,7 @@ const competitiveSites = {
       style: {
         color: "#F79F1B",
       },
-      profileLink: "https://leetcode.com/layman_brother/",
+      profileLink: "https://leetcode.com/aayamit/",
     },
     {
       siteName: "HackerRank",
@@ -336,7 +336,7 @@ const competitiveSites = {
       style: {
         color: "#2EC866",
       },
-      profileLink: "https://www.hackerrank.com/layman_brother",
+      profileLink: "https://www.hackerrank.com/aayamit",
     },
     {
       siteName: "Codechef",
@@ -344,7 +344,7 @@ const competitiveSites = {
       style: {
         color: "#5B4638",
       },
-      profileLink: "https://www.codechef.com/users/ashutosh_1919",
+      profileLink: "https://www.codechef.com/users/aayamit",
     },
     {
       siteName: "Codeforces",
@@ -352,7 +352,7 @@ const competitiveSites = {
       style: {
         color: "#1F8ACB",
       },
-      profileLink: "http://codeforces.com/profile/layman_brother",
+      profileLink: "http://codeforces.com/profile/aayamit",
     },
     {
       siteName: "Hackerearth",
@@ -360,7 +360,7 @@ const competitiveSites = {
       style: {
         color: "#323754",
       },
-      profileLink: "https://www.hackerearth.com/@ashutosh391",
+      profileLink: "https://www.hackerearth.com/@aayamit",
     },
     {
       siteName: "Kaggle",
@@ -368,7 +368,7 @@ const competitiveSites = {
       style: {
         color: "#20BEFF",
       },
-      profileLink: "https://www.kaggle.com/laymanbrother",
+      profileLink: "https://www.kaggle.com/aayamit",
     },
   ],
 };
@@ -376,30 +376,30 @@ const competitiveSites = {
 const degrees = {
   degrees: [
     {
-      title: "Indian Institute of Information Technology Kurnool",
-      subtitle: "B.Tech. in Computer Engineering",
+      title: "T.A. Pai Management Institute, Manipal",
+      subtitle: "MBA in Banking and Financial Services",
       logo_path: "iiitk_logo.png",
-      alt_name: "IIITDM Kurnool",
-      duration: "2016 - 2020",
+      alt_name: "TAPMI",
+      duration: "2022 - 2024",
       descriptions: [
-        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "⚡ In the first year, the program covers courses on accounting, finance, economics, and banking, along with basic knowledge about different functional areas of management. The second year offers students electives for specialization in banking or capital markets.",
+        "⚡ The program includes an integrated leadership program to develop essential leadership skills, and students have the opportunity to gain hands-on experience through the Bloomberg terminals and field visits to State Bank of India/Bombay Stock Exchange.",
+        "⚡ The course is aided by the state-of-the-art Bloomberg Lab, and upon completion, students can pursue managerial careers in the fields of banking and financial services with leading national and multinational banks and financial institutions.",
       ],
-      website_link: "http://iiitk.ac.in",
+      website_link: "https://www.tapmi.edu.in/",
     },
     {
-      title: "Indiana University Bloomington",
-      subtitle: "M.S. in Computer Science",
+      title: "Patna University",
+      subtitle: "B.S. in Physics",
       logo_path: "iu_logo.png",
-      alt_name: "Indiana University Bloomington",
-      duration: "2021 - 2023",
+      alt_name: "Patna University",
+      duration: "2018 - 2021",
       descriptions: [
-        "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-        "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-        "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+        "⚡  I have a strong foundation in Physics, which is essential for understanding the principles of Quantum Mechanics.",
+        "⚡  I have taken specialized courses related to Quantum Mechanics, such as Quantum Field Theory, Quantum Computing, and Quantum Information.",
+        "⚡ Additionally, I have completed research projects related to Quantum Mechanics, such as simulations of quantum systems and experiments on quantum entanglement.",
       ],
-      website_link: "https://www.indiana.edu/",
+      website_link: "https://pup.ac.in/",
     },
   ],
 };
