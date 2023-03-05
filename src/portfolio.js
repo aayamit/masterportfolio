1933a4250a430879cd892c2ece5db0664731c48f
aayamit <aayamit@gmail.com>
2023-03-06 03:48:52 +0530

    now

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,9 +23,9 @@ const greeting = {
   logo_name: "AmitKumar",
   nickname: "crypto_enthusiast",
   subTitle:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+    "I am excited to apply for an internship at your company and eager to contribute my skills and knowledge to the company. I am confident that this opportunity will provide me with invaluable experience and help me grow both personally and professionally. Let's team up to make a positive impact together!",
   resumeLink:
-    "https://drive.google.com/file/d/1064lqtkf3diCM0qm8IVSyszZ49mBZBZ9/view?usp=sharing",
+    "https://drive.google.com/file/d/1L7MuLxbd6-Ld3g3mpEHofoujbHQXNjqC/view?usp=sharing",
   portfolio_repository: "https://github.com/aayamit",
   githubProfile: "https://github.com/aayamit",
 };
@@ -87,6 +87,31 @@ const socialMediaLinks = [
 const skills = {
   data: [
     {
+      title: "Equity Analysis",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Strong analytical skills: I can analyze and interpret large amounts of financial data.",
+        "⚡ Financial Modeling Skills: I am able to able to build complex financial models that can help them forecast future earnings, cash flows, and other important metrics.",
+        "⚡ Ability to perform thorough research and analysis of markets and companies: I have written various reports.",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "Excel",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+      ],
+    },
+    {
       title: "Data Science & AI",
       fileName: "DataScienceImg",
       skills: [
@@ -127,105 +152,15 @@ const skills = {
         },
       ],
     },
+
     {
-      title: "Full Stack Development",
-      fileName: "FullStackImg",
-      skills: [
-        "⚡ Building responsive website front end using React-Redux",
-        "⚡ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
-        "⚡ Creating application backend in Node, Express & Flask",
-      ],
-      softwareSkills: [
-        {
-          skillName: "HTML5",
-          fontAwesomeClassname: "simple-icons:html5",
-          style: {
-            color: "#E34F26",
-          },
-        },
-        {
-          skillName: "CSS3",
-          fontAwesomeClassname: "fa-css3",
-          style: {
-            color: "#1572B6",
-          },
-        },
-        {
-          skillName: "Sass",
-          fontAwesomeClassname: "simple-icons:sass",
-          style: {
-            color: "#CC6699",
-          },
-        },
-        {
-          skillName: "JavaScript",
-          fontAwesomeClassname: "simple-icons:javascript",
-          style: {
-            backgroundColor: "#000000",
-            color: "#F7DF1E",
-          },
-        },
-        {
-          skillName: "ReactJS",
-          fontAwesomeClassname: "simple-icons:react",
-          style: {
-            color: "#61DAFB",
-          },
-        },
-        {
-          skillName: "NodeJS",
-          fontAwesomeClassname: "simple-icons:node-dot-js",
-          style: {
-            color: "#339933",
-          },
-        },
-        {
-          skillName: "NPM",
-          fontAwesomeClassname: "simple-icons:npm",
-          style: {
-            color: "#CB3837",
-          },
-        },
-        {
-          skillName: "Yarn",
-          fontAwesomeClassname: "simple-icons:yarn",
-          style: {
-            color: "#2C8EBB",
-          },
-        },
-        {
-          skillName: "Gatsby",
-          fontAwesomeClassname: "simple-icons:gatsby",
-          style: {
-            color: "#663399",
-          },
-        },
-        {
-          skillName: "Flutter",
-          fontAwesomeClassname: "simple-icons:flutter",
-          style: {
-            color: "#02569B",
-          },
-        },
-      ],
-    },
-    {
-      title: "Cloud Infra-Architecture",
+      title: "Fintech Enthusiast",
       fileName: "CloudInfraImg",
       skills: [
-        "⚡ Experience working on multiple cloud platforms",
-        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
-        "⚡ Deploying deep learning models on cloud to use on mobile devices",
-        "⚡ Setting up streaming jobs from DB to Server or vice-versa on GCP and AWS",
+        "⚡ Extensive knowledge of cryptocurrencies and blockchain technology, with a keen interest in staying up-to-date on the latest trends and developments in the field.",
+        "⚡ Proficiency in analyzing market trends and making informed investment decisions related to cryptocurrencies and other digital assets.",
       ],
       softwareSkills: [
-        {
-          skillName: "GCP",
-          fontAwesomeClassname: "simple-icons:googlecloud",
-          style: {
-            color: "#4285F4",
-          },
-        },
         {
           skillName: "AWS",
           fontAwesomeClassname: "simple-icons:amazonaws",
@@ -273,45 +208,6 @@ const skills = {
           fontAwesomeClassname: "simple-icons:kubernetes",
           style: {
             color: "#326CE5",
-          },
-        },
-      ],
-    },
-    {
-      title: "UI/UX Design",
-      fileName: "DesignImg",
-      skills: [
-        "⚡ Designing highly attractive user interface for mobile and web applications",
-        "⚡ Customizing logo designs and building logos from scratch",
-        "⚡ Creating the flow of application functionalities to optimize user experience",
-      ],
-      softwareSkills: [
-        {
-          skillName: "Adobe XD",
-          fontAwesomeClassname: "simple-icons:adobexd",
-          style: {
-            color: "#FF2BC2",
-          },
-        },
-        {
-          skillName: "Figma",
-          fontAwesomeClassname: "simple-icons:figma",
-          style: {
-            color: "#F24E1E",
-          },
-        },
-        {
-          skillName: "Adobe Illustrator",
-          fontAwesomeClassname: "simple-icons:adobeillustrator",
-          style: {
-            color: "#FF7C00",
-          },
-        },
-        {
-          skillName: "Inkscape",
-          fontAwesomeClassname: "simple-icons:inkscape",
-          style: {
-            color: "#000000",
           },
         },
       ],
@@ -409,7 +305,7 @@ const certifications = {
     {
       title:
         "Decentralized Finance (DeFi): The Future of Finance Specialization",
-      subtitle: "- Andrew Ng",
+      subtitle: "",
       logo_path: "stanford_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/specialization/certificate/6QX9M8BA5Q6U",
@@ -418,7 +314,7 @@ const certifications = {
     },
     {
       title: "Google Data Analytics Specialization",
-      subtitle: "- Andrew Ng",
+      subtitle: "",
       logo_path: "deeplearning_ai_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/specialization/certificate/E79ND42XFAUE",
@@ -427,7 +323,7 @@ const certifications = {
     },
     {
       title: "Risk Management Specialization",
-      subtitle: "- GCP Training",
+      subtitle: "",
       logo_path: "google_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/specialization/certificate/2HFVGLWJSU37",
@@ -436,7 +332,7 @@ const certifications = {
     },
     {
       title: "Excel Skills for Business Specialization",
-      subtitle: "- Alex Aklson",
+      subtitle: "",
       logo_path: "ibm_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/specialization/certificate/4WDMFG52JQLW",
@@ -445,7 +341,7 @@ const certifications = {
     },
     {
       title: "Stock Valuation with Dividend Discount Model",
-      subtitle: "- Kim Akers",
+      subtitle: "",
       logo_path: "microsoft_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/certificate/SVE8JATC9G2B",
@@ -454,7 +350,7 @@ const certifications = {
     },
     {
       title: "Introduction to Valuation with WACC",
-      subtitle: "- Romeo Kienzler",
+      subtitle: "",
       logo_path: "ibm_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/certificate/B2BBNQWGT22M",
@@ -463,7 +359,7 @@ const certifications = {
     },
     {
       title: "Bloomberg Market Concepts",
-      subtitle: "- GCP Training",
+      subtitle: "",
       logo_path: "google_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
@@ -472,7 +368,7 @@ const certifications = {
     },
     {
       title: "NISM-Series- VIII: Equity Derivatives Certificate",
-      subtitle: "- Laurence Moroney",
+      subtitle: "",
       logo_path: "deeplearning_ai_logo.png",
       certificate_link:
         "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
